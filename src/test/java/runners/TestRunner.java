cc957fd7b4f183e1978ba5a40d122227ea9ef595
velumanizeemart <velumani@zeemart.asia>
2022-02-14 13:21:31 +0530

    testcasesadded

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -3,20 +3,19 @@ package runners;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions; 
  
     
 
-@RunWith(Cucumber.class)   
+@RunWith(Cucumber.class)    
 
 
 
 @CucumberOptions(strict = true, features = "src/test/resources/functionalTests/AdminLogin.feature",
 
-
-
 tags ="@Smoke",
-glue = { "stepDefinitions" },monochrome=true,dryRun=false,
+
+glue = { "stepDefinitions" },monochrome=true,dryRun=false, 
 plugin = {"json:target/cucumber.json"})
  
 public class TestRunner {
